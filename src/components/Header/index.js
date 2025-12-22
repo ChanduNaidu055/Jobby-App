844a059d7571b00b3aa4bb4c22df86458b3d1778
ChanduNaidu055 <chandunaidu.sunkara@gmail.com>
2025-12-22 10:49:13 +0000

    Initial Commit.

---
+++ src/components/Header/index.js
@@ -0,0 +1,44 @@
+import {Link, withRouter} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+import './index.css'
+
+const Header = props => {
+  const onClickLogout = () => {
+    const {history} = props
+    Cookies.remove('jwt_token')
+    history.replace('/login')
+  }
+
+  return (
+    <nav className="nav-header">
+      <Link to="/">
+        <img
+          className="website-logo"
+          src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+          alt="website logo"
+        />
+      </Link>
+      <ul className="nav-menu">
+        <Link to="/" className="nav-link">
+          <li>Home</li>
+        </Link>
+
+        <Link to="/jobs" className="nav-link">
+          <li>Jobs</li>
+        </Link>
+        <li>
+          <button
+            type="button"
+            className="logout-desktop-btn"
+            onClick={onClickLogout}
+          >
+            Logout
+          </button>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default withRouter(Header)
